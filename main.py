24088790b32ba0b4868cb523a0d13f5ddc6e843d
Luke Markham <luke.markham@astrazeneca.com>
2018-09-02 10:27:17 +0100

    AAA-Analytics

--- main.py
+++ main.py
@@ -2,25 +2,31 @@ from flask import Flask, render_template, flash, redirect
 
 from login_form_package.login_form import LoginForm
 from config import Config
-#from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-#bootstrap = Bootstrap(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/api/<string:name>', methods=['GET'])
 def apiHi(name):
     return name
 
+@app.route('/register')
+def register():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
+
+    return render_template('register.html', form=form)
+
 @app.route('/')
 @app.route('/index')
 def index():
-    """
-
-    :return:
-    """
     user = {'username': 'Luke'}
     posts = [
         {
@@ -32,7 +38,8 @@ def index():
             'body': 'I love cats more!'
         }
     ]
-    return render_template('landing_page_template/landing_page.html', title='Home', user=user, posts=posts)
+    #return render_template('landing_page_template/landing_page.html', title='Home', user=user, posts=posts)
+    return render_template('base.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -41,8 +48,9 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect('/')
-    return render_template('/login_template/login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='80')
+    #app.run(host='0.0.0.0', port='80')
+    app.run()
     app.debug = True
